08ffe6e4de4abe203b03d30c5d010d0587814b85
Jordan Chalupka <jordanchalupka@me.com>
2019-10-15 17:46:10 -0400

    adding sinon and test to ensure we are sending a "Hi!" message

--- tests/test_index.js
+++ tests/test_index.js
@@ -1,10 +1,18 @@
 // Import the dependencies for testing
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
 const app = require('..');
+const slack = require('../slack');
+
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
+
+// Spy on our slack interactions
+sinon.spy(slack);
+
 describe("App", () => {
     describe("POST /", () => {
         // Test to get all students record
@@ -16,6 +24,10 @@ describe("App", () => {
                     chai.expect(err).to.be.null;
                     // there should be a 200 status code
                     res.status.should.equal(200);
+                    // Should have sent slack a request to post a message
+                    chai.assert(slack.postMessage.calledOnce)
+                    // The message says "Hi!"
+                    chai.assert.include(slack.postMessage.getCall(0).args[0].form.text, "Hi!");
                     done();
                 });
         });
