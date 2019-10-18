ed8d1b09eb4fb08dc4ed69779964ccc910a8453e
dinnatruong <dinnatruong@gmail.com>
2019-10-17 22:31:55 -0400

    Created model and methods to save coffee shop preferences

---
+++ models/CoffeeShopPreference.js
@@ -0,0 +1,37 @@
+/**
+ * CoffeeShopPreference 
+ *
+ * Responsible for communication with the database regarding coffee shop preference
+ */
+
+const db = require('../db');
+
+class CoffeeShopPreference {
+    constructor(userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Saves a user's coffee shop preference
+     * @param {string} name
+     * @param {string} location
+     */
+    async saveCoffeeShopPreference(name, location) {
+        this.savedCoffeeShop = await db.saveCoffeeShopPreference(this.userId, name, location);
+    }
+
+    /**
+     * Prints details of the saved coffee shop preference
+     * @return {string} Formatted details of coffee shop preference
+     */
+    async printSavedCoffeeShopPreference() {
+        if (this.savedCoffeeShop) {
+            const shop = await db.getCoffeeShopPreferenceById(this.savedCoffeeShop.lastID);
+            return `${shop.name}, ${shop.location}`;
+        }
+
+        return "No coffee shop preferences saved."
+    }
+}
+
+module.exports = CoffeeShopPreference;
